2a48a58f238b61c847bdfda7c6af49fb4e6cbb7e
tayeeb-office <tayeeb.office@gmail.com>
2025-12-06 15:33:17 +0600

    commit 05

--- index.js
+++ index.js
@@ -34,15 +34,6 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/services", async (req, res) => {
-        const{category} = req.query;
-        const query = {}
-        if(category){
-            query.category = category;
-        }
-      const result = await petServices.find(query).toArray();
-      res.send(result);
-    });
 
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
